9a48a57fc424b70c0fbe3fc4e1a0cbbf95c86f4d
Z3nos <dtrnametags@gmail.com>
2025-06-09 21:37:56 -0500

    Added GLM And Started Messing With It

--- TestEngine/src/main.cpp
+++ TestEngine/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW\glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -8,6 +11,16 @@ void processInput(GLFWwindow* window);
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
+
+	// glm test
+	glm::vec4 vec(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f); // Identity matrix
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f)); // Translation
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f)); // Rotation
+	trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f)); // Scaling
+
+	vec = trans * vec;
+	std::cout << vec.x << ", " << vec.y << ", " << vec.z << std::endl;
 
 	// Initialize GLFW
 	glfwInit();
